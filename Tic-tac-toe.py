1b8e6d07ef8eb8475ae7f98c5ce0b9a913562539
Takshak-20 <107192761+Takshak-20@users.noreply.github.com>
2022-06-10 19:57:50 +0530

    Initial File
    A briefout of the initial messages.

---
+++ Tic-tac-toe.py
@@ -0,0 +1,18 @@
+#### TIC_TAC_TOE Game #### 
+
+
+def default():
+    # To set the initialization of game; printed at the start of every new game
+    print("\nWelcome!\n Let's get you started with TTT! (TIC_TAC_TOE >.<)")
+
+
+def rules():
+    print("This is the setup you'd be playing on; a few moments from now.")
+    print("Guessed the positions? Isn't the 3 X 3 board similar to the right side of your key board?😁\n")
+    print(" 7 | 8 | 9 ")
+    print("-----------")
+    print(" 4 | 5 | 6 ")
+    print("-----------")
+    print(" 1 | 2 | 3 ")
+    print("\nYou just have to input the position(1-9).")
+
